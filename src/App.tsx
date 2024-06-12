00d82c269757e152bdb9c65d34f362704f703242
Rezmii <bartoszrezmer20@gmail.com>
2024-06-12 15:14:56 +0200

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
-  return <div>test</div>;
+  return (
+    <Grid
+      templateAreas={`"header" "main"`}
+      gridTemplateRows={"auto 1fr"}
+      height="100vh"
+    >
+      <GridItem area="header"></GridItem>
+      <GridItem area="main"></GridItem>
+    </Grid>
+  );
 }
 
 export default App;
